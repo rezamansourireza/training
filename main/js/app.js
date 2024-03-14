3f5e0fd255c06330117fb736dbc7a478e148db03
rezamansourireza <rema49202@gmail.com>
2024-03-14 10:44:15 +0330

    part #17 start alphinejs

--- main/js/app.js
+++ main/js/app.js
@@ -1,6 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-      edge:"right"
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    // edge: "right"
   });
+
+
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
