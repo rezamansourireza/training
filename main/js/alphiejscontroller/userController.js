586141333b37d39b6fc762d73449e2531b899e94
rezamansourireza <rema49202@gmail.com>
2024-03-15 22:31:07 +0330

    part #24 pageination

--- main/js/alphiejscontroller/userController.js
+++ main/js/alphiejscontroller/userController.js
@@ -1,8 +1,12 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', () => ({
         users: [],
+        pageusers: [],
         isloading: false,
-        showaddmodal:false,
+        showaddmodal: false,
+        pagecount: 1,
+        itemscount: 4,
+        currentpage: 1,
         getUsers() {
             this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
@@ -11,6 +15,14 @@ document.addEventListener('alpine:init', () => {
             }).finally(() => {
                 this.isloading = false
             })
+        },
+
+        pagination() {
+            this.pagecount = Math.ceil(this.users.lenth / this.itemscount) // 10 / 4 = 3
+            let start = (this.currentpage * this.itemscount) - this.itemscount
+            let end = this.currentpage * this.itemscount
+            this.pageusers = this.users.slice(start,end)
+            console.log(this.pageusers);
         }
 
 
